afbcf9c4a6be61f537a394c23a8965a75c02142f
Volha Kuralenia <Volha_Kuralenia@epam.com>
2021-01-26 16:59:08 +0300

    2.0

--- CoordinatesXY/Program.cs
+++ CoordinatesXY/Program.cs
@@ -1,45 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace CoordinatesXY
 {
-    class Program
+    public struct coordinatesXY
     {
-        public static List<string> coordinatesList;
-
-        public static void ReadingLines()
+        public string X { set; get; }
+        public string Y { set; get; }
+        public override string ToString()
         {
-            string line = Console.ReadLine();
-            while (line != string.Empty)
-            {
-                coordinatesList.Add(line);
-                line = Console.ReadLine();
-            }
-        }
-        public static string FormatingLine(string line)
-        {
-            int commaIndex = line.IndexOf(',');
-            string coordinateX = line[..commaIndex++];
-            string coordinateY = line[commaIndex..];
-            line = $"X: {coordinateX} Y: {coordinateY}";
+            string line = $"X: {X} Y: {Y}";
             line = line.Replace('.', ',');
             return line;
         }
-        public static void PrintCoordinates()
+    }
+    class Program
+    {
+        private static coordinatesXY ReadingLine(string line)
         {
-            foreach (string item in coordinatesList)
-            {
-                string line = FormatingLine(item);
-                Console.WriteLine(line);
-            }
+            coordinatesXY coordinates = new coordinatesXY();
+            int commaIndex = line.IndexOf(',');
+            coordinates.X = line[..commaIndex++];
+            coordinates.Y = line[commaIndex..];
+            return coordinates;
         }
+
+
         static void Main()
         {
-            Console.WriteLine("Enter coordinates:");
-            coordinatesList = new List<string>();
-            ReadingLines();
-            Console.WriteLine("Result:");
-            PrintCoordinates();
+            coordinatesXY xY = ReadingLine(Console.ReadLine());
+            PrintResult(xY);
+
+            static void PrintResult(coordinatesXY finalCoordinates)
+            {
+                Console.WriteLine(finalCoordinates.ToString());
+            }
         }
     }
 }
